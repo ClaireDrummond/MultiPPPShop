628bccf4f0cc93bf123b03450bb6c5c736c88676
Claire Drummond <g00364781@gmit.ie>
2019-10-28 10:48:54 +0000

    INstalled Java and ran code

--- Java Files/person.java
+++ Java Files/person.java
@@ -1,27 +1,38 @@
 public class Person {
+
     private String name;
     private int age;
 
-    public Person(String n, int a){
+    // Create a constructor below:
+    public Person(String n, int a) { // arguments
         this.name = n;
         this.age = a;
     }
+    // To print out whats in the object we use a toString method:
     public String toString(){
-        return "NAME" + this.name + " AGE: " + this.age;
+        return "NAME:" + this.name + " AGE:" + this.age;
     }
 
-    public void setAge(int n){
-        if (n <0){
-            //this line is a comment, we want to finish the method and not modift the age
+    // we want to be able to CHANGE  the persons age etc so we make a mutator method:
+
+    public void setAge(int n){ // mutator that will be used on personAppRunner too
+        if (n <0) {
             return;
+
         }
-        this.age = n;
+        this.age = n; 
     }
+
     public static void main(String[] args){
-        Person a = new Person("John", 23);
-        Person b = new Person("Paul", 51);
-        System.out.printIn(a);
-        a.setAge(40);
-        System.out.printIn(b);
+        Person a = new Person("John",23);
+        Person b = new Person("Paul",51);
+        System.out.println(a);
+        a.age = -1; // test our setAge method
+        //a.setAge(40); // test our setAge method
+        System.out.println(a);
+        System.out.println(b);
     }
+
+
+
 }
